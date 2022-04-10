b4a2a8c61d5558aa3009579211335a3649f9ea00
Misha <46088515+MishaTY@users.noreply.github.com>
2022-04-10 11:21:22 -0400

    Add more checks while loading XML

--- Rectify11Installer/Core/Program.cs
+++ Rectify11Installer/Core/Program.cs
@@ -1,3 +1,5 @@
+using Rectify11Installer.Core;
+
 namespace Rectify11Installer
 {
     internal static class Program
@@ -13,6 +15,24 @@ namespace Rectify11Installer
             {
                 Application.VisualStyleState = System.Windows.Forms.VisualStyles.VisualStyleState.NoneEnabled;
                 MessageBox.Show("Rectify11 installer is only supported on Windows 11 or greater.", "Compatibility Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (!File.Exists("rectify11.xml"))
+            {
+                Application.VisualStyleState = System.Windows.Forms.VisualStyles.VisualStyleState.ClientAreaEnabled;
+                MessageBox.Show("Failure while loading: Rectify11.xml. The error (0x2) occured. The file cannot be found", "Initialization Failure", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            try
+            {
+                Patches.GetAll();
+            }
+            catch (Exception ex)
+            {
+                Application.VisualStyleState = System.Windows.Forms.VisualStyles.VisualStyleState.NonClientAreaEnabled;
+                MessageBox.Show("Failure while loading: Rectify11.xml. The error details can be found below.\n" + ex.ToString(), "Initialization Failure", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return;
             }
             _ = DarkMode.fnAllowDarkModeForApp(DarkMode.PreferredAppMode.AllowDark);
