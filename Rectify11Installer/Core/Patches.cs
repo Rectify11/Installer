d7ba638de84ff188bf246b43da87d7f8f22677d0
Misha <46088515+MishaTY@users.noreply.github.com>
2022-04-10 14:08:05 -0400

    More icons lol

--- Rectify11Installer/Core/Patches.cs
+++ Rectify11Installer/Core/Patches.cs
@@ -27,87 +27,90 @@ namespace Rectify11Installer.Core
                 {
                     if (patch != null)
                     {
-                        if (patch.Attributes == null)
+                        if (patch.NodeType != XmlNodeType.Comment)
                         {
-                            throw new Exception("<Patch> tag has no attributes!");
-                        }
-                        else
-                        {
-                            var packageNameAttrib = patch.Attributes["Package"];
-                            if (packageNameAttrib == null)
-                                throw new Exception("<Patch> tag missing Package attribute!");
-                            var archAttrib = patch.Attributes["Arch"];
-                            if (archAttrib == null)
-                                throw new Exception("<Patch> tag missing Arch attribute!");
-                            var hardLinkTargetAttrib = patch.Attributes["HardlinkTarget"];
-                            if (hardLinkTargetAttrib == null)
-                                throw new Exception("<Patch> tag missing HardlinkTarget attribute!");
-
-                            var packageName = packageNameAttrib.Value;
-                            var arch = archAttrib.Value;
-                            PackageArch archProper;
-                            if (arch == "amd64")
+                            if (patch.Attributes == null)
                             {
-                                archProper = PackageArch.Amd64;
+                                throw new Exception("<Patch> tag has no attributes!");
                             }
                             else
                             {
-                                throw new Exception("Unknown arch value: " + arch);
-                            }
+                                var packageNameAttrib = patch.Attributes["Package"];
+                                if (packageNameAttrib == null)
+                                    throw new Exception("<Patch> tag missing Package attribute!");
+                                var archAttrib = patch.Attributes["Arch"];
+                                if (archAttrib == null)
+                                    throw new Exception("<Patch> tag missing Arch attribute!");
+                                var hardLinkTargetAttrib = patch.Attributes["HardlinkTarget"];
+                                if (hardLinkTargetAttrib == null)
+                                    throw new Exception("<Patch> tag missing HardlinkTarget attribute!");
 
-                            var hardlinkTarget = hardLinkTargetAttrib.Value;
-
-                            var instructions = new List<PatchInstruction>();
-
-                            var commandsTag = patch.ChildNodes[0];
-                            if (commandsTag != null)
-                            {
-                                if (commandsTag.Name != "Commands")
+                                var packageName = packageNameAttrib.Value;
+                                var arch = archAttrib.Value;
+                                PackageArch archProper;
+                                if (arch == "amd64")
                                 {
-                                    throw new Exception("Unknown node under <Patch>: " + commandsTag.Name);
+                                    archProper = PackageArch.Amd64;
+                                }
+                                else
+                                {
+                                    throw new Exception("Unknown arch value: " + arch);
                                 }
 
-                                foreach (XmlNode item in commandsTag.ChildNodes)
+                                var hardlinkTarget = hardLinkTargetAttrib.Value;
+
+                                var instructions = new List<PatchInstruction>();
+
+                                var commandsTag = patch.ChildNodes[0];
+                                if (commandsTag != null)
                                 {
-                                    if (item != null)
+                                    if (commandsTag.Name != "Commands")
                                     {
-                                        if (item.Name != "Command")
+                                        throw new Exception("Unknown node under <Patch>: " + commandsTag.Name);
+                                    }
+
+                                    foreach (XmlNode item in commandsTag.ChildNodes)
+                                    {
+                                        if (item != null)
                                         {
-                                            throw new Exception("Unknown node under <Patch>: " + commandsTag.Name);
-                                        }
+                                            if (item.Name != "Command")
+                                            {
+                                                throw new Exception("Unknown node under <Patch>: " + commandsTag.Name);
+                                            }
 
-                                        if (item.Attributes != null)
-                                        {
-                                            var action = item.Attributes["action"];
-                                            if (action == null)
-                                                throw new Exception("Attribute: action is required under Command tag");
-                                            var resource = item.Attributes["resource"];
+                                            if (item.Attributes != null)
+                                            {
+                                                var action = item.Attributes["action"];
+                                                if (action == null)
+                                                    throw new Exception("Attribute: action is required under Command tag");
+                                                var resource = item.Attributes["resource"];
 
-                                            var mask = item.Attributes["mask"];
-                                            if (mask == null)
-                                                throw new Exception("Attribute: mask is required under Command tag");
+                                                var mask = item.Attributes["mask"];
+                                                if (mask == null)
+                                                    throw new Exception("Attribute: mask is required under Command tag");
 
-                                            string resourceProper = resource == null ? "" : resource.InnerText;
+                                                string resourceProper = resource == null ? "" : resource.InnerText;
 
-                                            instructions.Add(new PatchInstruction(action.InnerText, resourceProper, mask.InnerText));
+                                                instructions.Add(new PatchInstruction(action.InnerText, resourceProper, mask.InnerText));
+                                            }
+                                            else
+                                            {
+                                                throw new Exception("<Command> tag requires attributes");
+                                            }
                                         }
                                         else
                                         {
-                                            throw new Exception("<Command> tag requires attributes");
+                                            throw new Exception("Tag under <Commands> null!");
                                         }
                                     }
-                                    else
-                                    {
-                                        throw new Exception("Tag under <Commands> null!");
-                                    }
                                 }
-                            }
-                            else
-                            {
-                                throw new Exception("Tag under <Patch> null!");
-                            }
+                                else
+                                {
+                                    throw new Exception("Tag under <Patch> null!");
+                                }
 
-                            p.Add(new PatchDef(packageName, archProper, hardlinkTarget, instructions.ToArray()));
+                                p.Add(new PatchDef(packageName, archProper, hardlinkTarget, instructions.ToArray()));
+                            }
                         }
                     }
                 }
