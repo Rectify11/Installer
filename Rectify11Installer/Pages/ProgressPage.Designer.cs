52c3447868b9c80a99c6d9952bd63e9803658813
rounk-ctrl <70931017+rounk-ctrl@users.noreply.github.com>
2022-07-27 22:11:00 +0400

    fixed

--- Rectify11Installer/Pages/ProgressPage.Designer.cs
+++ Rectify11Installer/Pages/ProgressPage.Designer.cs
@@ -52,7 +52,6 @@
             // 
             // timer1
             // 
-            this.timer1.Enabled = true;
             this.timer1.Interval = 1000;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
