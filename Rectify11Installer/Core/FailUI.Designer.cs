269b62aa7c276bcaef6e4d5229e17342eb232cc4
Misha <46088515+MishaTY@users.noreply.github.com>
2022-07-28 11:37:50 -0400

    fix typo

--- Rectify11Installer/Core/FailUI.Designer.cs
+++ Rectify11Installer/Core/FailUI.Designer.cs
@@ -78,7 +78,7 @@
             this.label3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label3.Location = new System.Drawing.Point(3, 32);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(625, 45);
+            this.label3.Size = new System.Drawing.Size(619, 45);
             this.label3.TabIndex = 2;
             this.label3.Text = resources.GetString("label3.Text");
             // 
