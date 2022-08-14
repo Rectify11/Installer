76527075102581f2e58b237b8b1cb9d0beb5f01d
rounk-ctrl <70931017+rounk-ctrl@users.noreply.github.com>
2022-08-14 09:58:15 +0400

    initial work on installer

--- Rectify11Installer/Core/Installer.cs
+++ Rectify11Installer/Core/Installer.cs
@@ -1,7 +1,23 @@
-﻿namespace Rectify11Installer.Core
+﻿using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace Rectify11Installer.Core
 {
     public class Installer
     {
-
+        public static void CreatePendingXml()
+        {
+            new XDocument(
+                new XElement("PendingTransaction",
+                new XAttribute("Identifier", "0b3b509abaa9d80118ba000074118811"),
+                new XAttribute("Repair", "true"),
+                new XAttribute("Version", "3.1"),
+                    new XElement("Repaired"))
+                ).Save(Application.StartupPath + @"\pending.xml");
+        }
+        public static void Install()
+        {
+            CreatePendingXml();
+        }
     }
 }
