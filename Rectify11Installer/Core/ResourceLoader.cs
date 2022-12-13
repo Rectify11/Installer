0f78e744718a46f3e48cc7e17648b085080f3400
rounk-ctrl <70931017+rounk-ctrl@users.noreply.github.com>
2022-12-13 10:27:38 +0400

    fix language

--- Rectify11Installer/Core/ResourceLoader.cs
+++ Rectify11Installer/Core/ResourceLoader.cs
@@ -7,84 +7,78 @@ using System.Resources;
 
 namespace Rectify11Installer.Core
 {
-	internal class SingleAssemblyComponentResourceManager : ComponentResourceManager
-	{
-		private Type _contextTypeInfo;
-		private CultureInfo _neutralResourcesCulture;
+    class SingleAssemblyComponentResourceManager :
+    System.ComponentModel.ComponentResourceManager
+    {
+        private Type _contextTypeInfo;
+        private CultureInfo _neutralResourcesCulture;
 
-		public SingleAssemblyComponentResourceManager(Type t)
-			: base(t)
-		{
-			_contextTypeInfo = t;
-		}
+        public SingleAssemblyComponentResourceManager(Type t)
+            : base(t)
+        {
+            _contextTypeInfo = t;
+        }
 
-		protected override ResourceSet InternalGetResourceSet(CultureInfo culture,
-			bool createIfNotExists, bool tryParents)
-		{
-#pragma warning disable CS0618 // Type or member is obsolete
-			ResourceSet rs = (ResourceSet)ResourceSets[culture];
-#pragma warning restore CS0618 // Type or member is obsolete
-			if (rs == null)
-			{
-				Stream store = null;
-				string resourceFileName = null;
+        protected override ResourceSet InternalGetResourceSet(CultureInfo culture,
+            bool createIfNotExists, bool tryParents)
+        {
+            ResourceSet rs = (ResourceSet)this.ResourceSets[culture];
+            if (rs == null)
+            {
+                Stream store = null;
+                string resourceFileName = null;
 
-				//lazy-load default language (without caring about duplicate assignment in race conditions, no harm done);
-				if (_neutralResourcesCulture == null)
-				{
-					_neutralResourcesCulture =
-						GetNeutralResourcesLanguage(MainAssembly);
-				}
+                //lazy-load default language (without caring about duplicate assignment in race conditions, no harm done);
+                if (this._neutralResourcesCulture == null)
+                {
+                    this._neutralResourcesCulture =
+                        GetNeutralResourcesLanguage(this.MainAssembly);
+                }
 
-				// if we're asking for the default language, then ask for the
-				// invariant (non-specific) resources.
-				if (_neutralResourcesCulture.Equals(culture))
-				{
-					culture = CultureInfo.InvariantCulture;
-				}
+                // if we're asking for the default language, then ask for the
+                // invariant (non-specific) resources.
+                if (_neutralResourcesCulture.Equals(culture))
+                    culture = CultureInfo.InvariantCulture;
+                resourceFileName = GetResourceFileName(culture);
 
-				resourceFileName = GetResourceFileName(culture);
+                store = this.MainAssembly.GetManifestResourceStream(
+                    this._contextTypeInfo, resourceFileName);
 
-				store = MainAssembly.GetManifestResourceStream(
-					_contextTypeInfo, resourceFileName);
+                //If we found the appropriate resources in the local assembly
+                if (store != null)
+                {
+                    rs = new ResourceSet(store);
+                    //save for later.
+                    AddResourceSet(this.ResourceSets, culture, ref rs);
+                }
+                else
+                {
+                    rs = base.InternalGetResourceSet(culture, createIfNotExists, tryParents);
+                }
+            }
+            return rs;
+        }
 
-				//If we found the appropriate resources in the local assembly
-				if (store != null)
-				{
-					rs = new ResourceSet(store);
-					//save for later.
-#pragma warning disable CS0618 // Type or member is obsolete
-					AddResourceSet(ResourceSets, culture, ref rs);
-#pragma warning restore CS0618 // Type or member is obsolete
-				}
-				else
-				{
-					rs = base.InternalGetResourceSet(culture, createIfNotExists, tryParents);
-				}
-			}
-			return rs;
-		}
-
-		//private method in framework, had to be re-specified here.
-		private static void AddResourceSet(Hashtable localResourceSets,
-			CultureInfo culture, ref ResourceSet rs)
-		{
-			lock (localResourceSets)
-			{
-				ResourceSet objA = (ResourceSet)localResourceSets[culture];
-				if (objA != null)
-				{
-					if (!object.Equals(objA, rs))
-					{
-						rs.Dispose();
-						rs = objA;
-					}
-				}
-				else
-				{
-					localResourceSets.Add(culture, rs);
-				}
-			}
-		}
-	}
+        //private method in framework, had to be re-specified here.
+        private static void AddResourceSet(Hashtable localResourceSets,
+            CultureInfo culture, ref ResourceSet rs)
+        {
+            lock (localResourceSets)
+            {
+                ResourceSet objA = (ResourceSet)localResourceSets[culture];
+                if (objA != null)
+                {
+                    if (!object.Equals(objA, rs))
+                    {
+                        rs.Dispose();
+                        rs = objA;
+                    }
+                }
+                else
+                {
+                    localResourceSets.Add(culture, rs);
+                }
+            }
+        }
+    }
 }
--- Rectify11Installer/Properties/AssemblyInfo.cs
+++ Rectify11Installer/Properties/AssemblyInfo.cs
@@ -34,4 +34,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("3.0.0.0")]
 [assembly: AssemblyFileVersion("3.0.0.0")]
-[assembly: NeutralResourcesLanguage("en-US")]
+[assembly: NeutralResourcesLanguage("")]
