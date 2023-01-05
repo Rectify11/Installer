c16071cce1ccfb28665db6adb5d409bd1a7909cb
ojask <techno.mind69@gmail.com>
2023-01-05 13:28:54 +0530

    more control center stuff

---
+++ Rectify11ControlCenter/Controls.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace Rectify11ControlCenter
+{
+    public static class Controls
+    {
+        //PreviewPane
+        public static string osV = "OS Version: Windows NT Build " + Environment.OSVersion.Version.ToString();
+        public static string userN = "Username: " + System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+        public static string CumterName = "PC Name: " + Environment.MachineName;
+        public static string theme = "Theme: " + "Not implemented";
+        public static string r11Version = "Rectify11 Version: " + Assembly.GetEntryAssembly().GetName().Version.ToString();
+        public static Image deskimg()
+        {
+            string pathWallpaper = "";
+            RegistryKey regKey = Registry.CurrentUser.OpenSubKey("Control Panel\\Desktop", false);
+            pathWallpaper = regKey.GetValue("WallPaper").ToString();
+            regKey.Close();
+            Image image = Image.FromFile(pathWallpaper);
+            return image;
+        }
+
+    }
+}
--- Rectify11ControlCenter/Properties/AssemblyInfo.cs
+++ Rectify11ControlCenter/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Rectify11ControlCenter")]
+[assembly: AssemblyTitle("Rectify11 Control Center")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Rectify11ControlCenter")]
+[assembly: AssemblyProduct("Rectify11 Control Center")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("3.0.24.0")]
+[assembly: AssemblyFileVersion("3.0.24.0")]
