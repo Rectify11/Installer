3dc341e5f45de806dea60ceb265eb7bb7e6b8abf
ojask <techno.mind69@gmail.com>
2023-04-20 17:15:47 +0530

    Version increment and updated newFiles.txt
    Former-commit-id: 7a4f69fd2a83f3c63d6d26eec9448d008d30368a

--- Rectify11Installer/Properties/AssemblyInfo.cs
+++ Rectify11Installer/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.0.24.0")]
-[assembly: AssemblyFileVersion("3.0.24.0")]
+[assembly: AssemblyVersion("3.0.30.0")]
+[assembly: AssemblyFileVersion("3.0.30.0")]
 [assembly: NeutralResourcesLanguage("")]
