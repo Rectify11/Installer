a93ad7bf3ef7708eccd6a65697b991f1c7c0f197
WinExperiments <91428431+WinExperiments@users.noreply.github.com>
2023-08-02 20:10:05 +0300

    Segoe UI Variable gets applied after installation

--- Rectify11Installer/Core/Installer.cs
+++ Rectify11Installer/Core/Installer.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Globalization;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows.Forms;
