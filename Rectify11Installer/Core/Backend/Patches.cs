3d18d747afa37cbd4c3443a9c7f997c3bee97d54
rounk-ctrl <70931017+rounk-ctrl@users.noreply.github.com>
2023-09-28 00:27:52 +0400

    fix conflict

--- Rectify11Installer/Core/Backend/Patches.cs
+++ Rectify11Installer/Core/Backend/Patches.cs
@@ -6,113 +6,116 @@ using System.IO;
 using System.Xml.Schema;
 using System.Xml.Serialization;
 
-#region Parser
-public class PatchesParser
+namespace Rectify11Installer.Core
 {
-    #region Public Methods
-    public static Patches GetAll()
+    #region Parser
+    public class PatchesParser
     {
-        XmlSerializer ser = new(typeof(Patches));
-        Patches patches;
-        using (var reader = new StringReader(Rectify11Installer.Properties.Resources.rectify11xml))
+        #region Public Methods
+        public static Patches GetAll()
         {
-            patches = (Patches)ser.Deserialize(reader);
+            XmlSerializer ser = new(typeof(Patches));
+            Patches patches;
+            using (var reader = new StringReader(Rectify11Installer.Properties.Resources.rectify11xml))
+            {
+                patches = (Patches)ser.Deserialize(reader);
+            }
+            return patches;
         }
-        return patches;
+        #endregion
+    }
+    #endregion
+    #region Deserializer Class
+
+    [GeneratedCodeAttribute("xsd", "4.6.1055.0")]
+    [SerializableAttribute()]
+    [DebuggerStepThroughAttribute()]
+    [DesignerCategoryAttribute("code")]
+    [XmlTypeAttribute(AnonymousType = true)]
+    [XmlRootAttribute(Namespace = "", IsNullable = false)]
+    public partial class Patches
+    {
+
+        private PatchesPatch[] itemsField;
+        [XmlElementAttribute("Patch", Form = XmlSchemaForm.Unqualified)]
+        public PatchesPatch[] Items
+        {
+            get => this.itemsField;
+            set => this.itemsField = value;
+        }
+    }
+
+    [GeneratedCodeAttribute("xsd", "4.6.1055.0")]
+    [SerializableAttribute()]
+    [DebuggerStepThroughAttribute()]
+    [DesignerCategoryAttribute("code")]
+    [XmlTypeAttribute(AnonymousType = true)]
+    public partial class PatchesPatch
+    {
+        private string muiField;
+        private string maskField;
+        private string x86Field;
+        private string hardlinkTargetField;
+        private string disableOnSafeModeField;
+        private string IgnoreField;
+        private string MinVersionField;
+        private string MaxVersionField;
+
+        [XmlAttributeAttribute()]
+        public string Mui
+        {
+            get => this.muiField;
+            set => this.muiField = value;
+        }
+
+        [XmlAttributeAttribute()]
+        public string mask
+        {
+            get => this.maskField;
+            set => this.maskField = value;
+        }
+
+        [XmlAttributeAttribute()]
+        public string x86
+        {
+            get => this.x86Field;
+            set => this.x86Field = value;
+        }
+
+        [XmlAttributeAttribute()]
+        public string HardlinkTarget
+        {
+            get => this.hardlinkTargetField;
+            set => this.hardlinkTargetField = value;
+        }
+
+        [XmlAttributeAttribute()]
+        public string DisableOnSafeMode
+        {
+            get => this.disableOnSafeModeField;
+            set => this.disableOnSafeModeField = value;
+        }
+
+        [XmlAttributeAttribute()]
+        public string Ignore
+        {
+            get => this.IgnoreField;
+            set => this.IgnoreField = value;
+        }
+
+        [XmlAttributeAttribute()]
+        public string MinVersion
+        {
+            get => this.MinVersionField;
+            set => this.MinVersionField = value;
+        }
+
+        [XmlAttributeAttribute()]
+        public string MaxVersion
+        {
+            get => this.MaxVersionField;
+            set => this.MaxVersionField = value;
+        }
     }
     #endregion
 }
-#endregion
-#region Deserializer Class
-
-[GeneratedCodeAttribute("xsd", "4.6.1055.0")]
-[SerializableAttribute()]
-[DebuggerStepThroughAttribute()]
-[DesignerCategoryAttribute("code")]
-[XmlTypeAttribute(AnonymousType = true)]
-[XmlRootAttribute(Namespace = "", IsNullable = false)]
-public partial class Patches
-{
-
-    private PatchesPatch[] itemsField;
-    [XmlElementAttribute("Patch", Form = XmlSchemaForm.Unqualified)]
-    public PatchesPatch[] Items
-    {
-        get => this.itemsField;
-        set => this.itemsField = value;
-    }
-}
-
-[GeneratedCodeAttribute("xsd", "4.6.1055.0")]
-[SerializableAttribute()]
-[DebuggerStepThroughAttribute()]
-[DesignerCategoryAttribute("code")]
-[XmlTypeAttribute(AnonymousType = true)]
-public partial class PatchesPatch
-{
-    private string muiField;
-    private string maskField;
-    private string x86Field;
-    private string hardlinkTargetField;
-    private string disableOnSafeModeField;
-    private string IgnoreField;
-    private string MinVersionField;
-    private string MaxVersionField;
-
-    [XmlAttributeAttribute()]
-    public string Mui
-    {
-        get => this.muiField;
-        set => this.muiField = value;
-    }
-
-    [XmlAttributeAttribute()]
-    public string mask
-    {
-        get => this.maskField;
-        set => this.maskField = value;
-    }
-
-    [XmlAttributeAttribute()]
-    public string x86
-    {
-        get => this.x86Field;
-        set => this.x86Field = value;
-    }
-
-    [XmlAttributeAttribute()]
-    public string HardlinkTarget
-    {
-        get => this.hardlinkTargetField;
-        set => this.hardlinkTargetField = value;
-    }
-
-    [XmlAttributeAttribute()]
-    public string DisableOnSafeMode
-    {
-        get => this.disableOnSafeModeField;
-        set => this.disableOnSafeModeField = value;
-    }
-
-    [XmlAttributeAttribute()]
-    public string Ignore
-    {
-        get => this.IgnoreField;
-        set => this.IgnoreField = value;
-    }
-
-    [XmlAttributeAttribute()]
-    public string MinVersion
-    {
-        get => this.MinVersionField;
-        set => this.MinVersionField = value;
-    }
-
-    [XmlAttributeAttribute()]
-    public string MaxVersion
-    {
-        get => this.MaxVersionField;
-        set => this.MaxVersionField = value;
-    }
-}
-#endregion
